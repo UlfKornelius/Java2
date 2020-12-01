1345b6b9332bff13a38cbba6215fc44a9678a2bd
ulfkornelius <wushu.kgd@mail.ru>
2020-12-01 12:27:15 +0200

    Homework №1

---
+++ src/java2/lesson1/BackgroundCanvas.java
@@ -0,0 +1,26 @@
+package ru.bondarev.java2.lesson1;
+
+import java.awt.*;
+
+
+public class BackgroundCanvas extends Sprite {
+    private float time;
+    private static final float AMPLITUDE = 255f / 2f;
+    private Color color;
+
+    @Override
+    public void update(GameCanvas gameCanvas, float deltaTime) {
+        time += deltaTime;
+        int red = Math.round(AMPLITUDE + AMPLITUDE * (float) Math.sin(time * 2f));
+        int green = Math.round(AMPLITUDE + AMPLITUDE * (float) Math.sin(time * 3f));
+        int blue = Math.round(AMPLITUDE + AMPLITUDE * (float) Math.sin(time));
+        color = new Color(red, green, blue);
+    }
+
+    @Override
+    void render(GameCanvas gameCanvas, Graphics g) {
+        gameCanvas.setBackground(color);
+    }
+
+
+}
